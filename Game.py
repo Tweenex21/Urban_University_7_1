ba0b9d37bfa593ba2dc0cd256fd3c2328d7ba5bd
Tweenex <tweenex@mail.ru>
2024-07-08 22:20:38 +0300

    Praktic

--- Game.py
+++ Game.py
@@ -7,14 +7,30 @@ class Ball(arcade.Sprite):
     def __init__(self):
         super().__init__("ball.png", 0.03)
         self.change_x = 1
+        self.change_y = 1
+
     def update(self):
         self.center_x += self.change_x
         self.center_y += self.change_y
+        if self.right >= SCREEN_WIDTH:
+            self.change_x = -self.change_x
+        if self.left <= 0:
+            self.change_x = -self.change_x
+        if self.top >= SCREEN_HEIGHT:
+            self.change_y = -self.change_y
+        if self.bottom <= 0:
+            self.change_y = -self.change_y
 
 
 class Bar(arcade.Sprite):
     def __init__(self):
         super().__init__("bar.png", 0.2)
+    def update(self):
+        self.center_x += self.change_x
+        if self.right >= SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left <= 0:
+            self.left = 0
 class Game(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
@@ -32,7 +48,18 @@ class Game(arcade.Window):
         self.bar.draw()
         self.ball.draw()
     def update(self, delta):
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.change_y = -self.ball.change_y
         self.ball.update()
+        self.bar.update()
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.RIGHT:
+            self.bar.change_x = 5
+        if key == arcade.key.LEFT:
+            self.bar.change_x = -5
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.bar.change_x = 0
 
 
 if __name__ == '__main__':

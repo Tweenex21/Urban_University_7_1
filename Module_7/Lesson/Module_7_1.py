e2c81e68ec8453c32ac2213c2030a170fd963d25
Tweenex21 <tweenex@mail.ru>
2024-09-19 19:42:47 +0300

    Update Module_7_1.py

--- Module_7/Lesson/Module_7_1.py
+++ Module_7/Lesson/Module_7_1.py
@@ -1,39 +1,39 @@
-
 class Product:
-    def __init__(self, name, weight, category):
+
+    def __init__(self, name: str, weight: float, category: str):
         self.name = name
         self.weight = weight
         self.category = category
 
     def __str__(self):
-        return f"{self.name}, {self.weight}, {self.category}"
+        return f'{self.name}, {self.weight}, {self.category}'
 
 
 class Shop:
-    def __init__(self):
-        self.__file_name = 'products.txt'
+    __file_name = 'products.txt'
 
     def get_products(self):
-        with open(self.__file_name, 'r') as file:
-            products = file.read()
-        return products
+        file = open(self.__file_name, 'r')
+        return file.read()
 
     def add(self, *products):
-        existing_products = self.get_products().split('\n')
-        with open(self.__file_name, 'a') as file:
-            for product in products:
-                product_name = product.name
-                if any(product_name in line for line in existing_products):
-                    print(f'Продукт {product_name} уже есть в магазине')
-                else:
-                    file.write(str(product) + '\n')
+        for product in products:
+            file = open(self.__file_name, 'a')
+            current_products = self.get_products()
+            if product.name in current_products:
+                print(f'Продукт {product.name} уже есть в магазине')
+                file.close()
+            else:
+                file.write(str(product) + '\n')
+                file.close()
+
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
